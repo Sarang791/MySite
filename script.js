912f626a544cf1b595a280bd33c43113c3906368
Sarang791 <114277936+Sarang791@users.noreply.github.com>
2023-12-26 10:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 $(".navbar>img").click(function (){
     $(".menubar").toggleClass("menubarGo");
 })
+
+function downloadResume() {
+            
+    // Replace 'path/to/your/resume.pdf' with the actual path to your resume file
+    var resumePath = './Sarang More.pdf';
+    
+    // Creating an invisible link element
+    var link = document.createElement('a');
+    link.href = resumePath;
+    link.download = 'SarangMore.pdf';
+
+    // Appending the link to the document
+    document.body.appendChild(link);
+
+    // Triggering the click event to start the download
+    link.click();
+
+    // Removing the link from the document
+    document.body.removeChild(link);
+}
